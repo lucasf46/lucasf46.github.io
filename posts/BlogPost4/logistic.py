d5ff1f2c0336859390d95e07d6cad5659a82f01a
Lucas Flemming <lflemming@middlebury.edu>
2024-05-14 11:42:21 -0400

    Need to add discussion paragraph

--- posts/BlogPost4/logistic.py
+++ posts/BlogPost4/logistic.py
@@ -60,6 +60,18 @@ class LogisticRegression(LinearModel):
 
     def logistic_loss(self, X, y):
 
+        """
+        Calculate the logistic loss for a dataset given its feature matrix X and labels y. 
+
+        ARGUMENTS:
+            X, torch.Tensor: The feature matrix where each row is a feature vector. The size of X is (n, p),
+                               where n is the number of data points and p is the number of features.
+            y, torch.Tensor: The true labels for each data point in X.
+
+        RETURNS:
+            torch.Tensor: The mean logistic loss for the given data.
+        """
+
         s_i = self.score(X)
         sigmoid_si = torch.sigmoid(s_i)
         loss = (-y * torch.log(sigmoid_si)) - ((1 - y) * torch.log(1 - sigmoid_si))
@@ -68,7 +80,21 @@ class LogisticRegression(LinearModel):
 
     def logistic_grad(self, X, y):
 
+        """
+        Compute the gradient of the logistic loss function.
+
+        ARGUMENTS:
+            X, torch.Tensor: The feature matrix where each row is a feature vector. The size of X is (n, p),
+                               where n is the number of data points and p is the number of features.
+            y, torch.Tensor: The true labels for each data point in X.
+
+        RETURNS:
+            torch.Tensor: The gradient of the logistic loss.
+        """
+
         s_i = self.score(X)
+        # [:,None] converts a tensor v with shape (n,) into a tensor v_ with shape (n,1)
+        # borrowed from Logistic Regression blog post page
         delta = ((torch.sigmoid(s_i) - y)[:,None]* X)
         gradient = torch.mean(delta, dim = 0)
 
@@ -81,6 +107,20 @@ class GradientDescentOptimizer:
 
 
     def step(self, X, y, alpha, beta):
+
+        """
+        Perform one step of gradient descent optimization on the logistic regression model's parameters.
+
+        ARGUMENTS:
+            X, torch.Tensor: The feature matrix. Size of X is (n, p), where n is the number of data points,
+                              and p is the number of features.
+            y, torch.Tensor: The true labels for each data point in X. 
+            alpha, float: The learning rate, a scalar that scales the gradient.
+            beta, float: The momentum factor, a scalar that scales the contribution of the previous weight update.
+
+        RETURNS:
+            torch.Tensor: The logistic loss after updating the model's parameters.
+        """
 
         gradient = self.model.logistic_grad(X, y)
         weight = self.model.w
